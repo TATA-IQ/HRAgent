c4d4c2139d4235a3d9fda255e6b99820ba6c9d3f
Jumin kamki <jkamki@tataiq.com>
2025-01-23 05:30:25 +0000

    document processed

--- data_ingestor.py
+++ data_ingestor.py
@@ -2,6 +2,6 @@ from store.dbstore import DBCreation
 import multiprocessing as mp
 
 if __name__ == "__main__":
-    db=DBCreation(r"D:/HRAgent/Policies/","http://172.16.0.178:8010")
+    db=DBCreation(r"Policies/","http://172.16.0.178:8010")
     db.create_knowledge_semantic_spacy()
     
